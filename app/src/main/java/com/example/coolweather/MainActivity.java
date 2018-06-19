f0e8cafb182d26d6dfa7d706ea072ea1edd72b32
YJL <1015586146@qq.com>
2018-06-19 20:34:19 +0800

    lkkkk

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -4,8 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-        private  String  aaa;
-        private  int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
